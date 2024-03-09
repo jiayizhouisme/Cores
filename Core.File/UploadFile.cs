97bebb02bc2f720384937a01f55a07a34f4bcbd0
zhoujiayi <zhoujiayi7@gmail.com>
2024-03-09 20:19:06 +0800

    新增文件处理

---
+++ Core.File/UploadFile.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Net.Http.Headers;
+using Microsoft.AspNetCore.WebUtilities;
+using SampleApp.Utilities;
+using Core.File.Model;
+
+namespace Core.File
+{
+    public class UploadFile
+    {
+        public static async Task UploadPhysical(IHttpContextAccessor httpContext,FileChunk fileChunk)
+        {
+            int size = fileChunk.Size;
+            if (fileChunk.Total < fileChunk.Size)
+            {
+                size = fileChunk.Total;
+            }
+            var Request = httpContext.HttpContext.Request;
+            var boundary = MultipartRequestHelper.GetBoundary(
+                MediaTypeHeaderValue.Parse(Request.ContentType),
+                size);
+            var reader = new MultipartReader(boundary, Request.Body);
+            var section = await reader.ReadNextSectionAsync();
+
+            while (section != null)
+            {
+                var hasContentDispositionHeader =
+                    ContentDispositionHeaderValue.TryParse(
+                        section.ContentDisposition, out var contentDisposition);
+
+                if (hasContentDispositionHeader)
+                {
+                    // This check assumes that there's a file
+                    // present without form data. If form data
+                    // is present, this method immediately fails
+                    // and returns the model error.
+                    if (!MultipartRequestHelper
+                        .HasFileContentDisposition(contentDisposition))
+                    {
+                    }
+                    else
+                    {
+                        // Don't trust the file name sent by the client. To display
+                        // the file name, HTML-encode the value.
+                        var trustedFileNameForDisplay = WebUtility.HtmlEncode(
+                                contentDisposition.FileName.Value);
+                        var trustedFileNameForFileStorage = Path.GetRandomFileName();
+
+                        var streamedFileContent = await FileHelpers.ProcessStreamedFile(
+                            section, contentDisposition, size);
+
+
+                        using (var targetStream = System.IO.File.Create(
+                            Path.Combine("D:/", trustedFileNameForFileStorage)))
+                        {
+                            await targetStream.WriteAsync(streamedFileContent);
+                        }
+                    }
+                }
+
+                // Drain any remaining section body that hasn't been consumed and
+                // read the headers for the next section.
+                section = await reader.ReadNextSectionAsync();
+            }
+        }
+        }
+    }
