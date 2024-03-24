1c6c13ec3e9ac9ba451e207b6792249f38c7db2a
jiayizhouisme <zhoujiayi7@gmail.com>
2024-03-24 12:05:01 +0800

    #

--- Core.MiddelWares/CacheFilter.cs
+++ Core.MiddelWares/CacheFilter.cs
@@ -9,7 +9,7 @@ namespace Core.MiddelWares
     {
         protected readonly ICacheOperation _cache;
         protected string suffixed;
-        public static int _expireTime = 30;
+        public const int _expireTime = 30;
 
         public CacheFilter(ICacheOperation _cache)
         {
