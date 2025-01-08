97c86fb519c654524b869870b5ae85c6159a1c7c
zhoujiayi7 <zhoujiayi7@gmail.com>
2025-01-08 11:08:49 +0800

    #

--- Core.MiddelWares/RateLimitPolicy.cs
+++ Core.MiddelWares/RateLimitPolicy.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Core.Auth;
+using Furion;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.RateLimiting;
-using System.Threading.Tasks;
 
 namespace Core.MiddelWares
 {
@@ -42,6 +39,38 @@ namespace Core.MiddelWares
 
             });
         }
+
+        /// <summary>
+        /// 请求上下文中间件
+        /// </summary>
+        /// <param name="app"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddUserRateLimiter(this IServiceCollection services,
+            string policyName = "user_rate_policy",
+            int PermitLimit = 3, int _window = 60,
+            QueueProcessingOrder _QueueProcessingOrder = QueueProcessingOrder.OldestFirst,
+            int _queueLimit = 0)
+        {
+            return services.AddRateLimiter(options =>
+            {
+                options.AddPolicy(policyName: policyName, httpcontext =>
+                {
+                    string userId = App.GetService<IHttpContextUser>().ID;
+                    return RateLimitPartition.GetFixedWindowLimiter(partitionKey: userId, _ => new
+                        FixedWindowRateLimiterOptions
+                    {
+                        PermitLimit = PermitLimit,
+                        Window = TimeSpan.FromSeconds(_window),
+                        QueueProcessingOrder = _QueueProcessingOrder,
+                        QueueLimit = _queueLimit
+                    });
+                });
+
+
+            });
+        }
+
+        
     }
     
 }
